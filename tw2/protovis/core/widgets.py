042ce5ea3bc2abd4d4214f07e0f90d9122f1a855
Ralph Bean <ralph.bean@gmail.com>
2010-10-07 12:00:43 -0400

    declarative mapping

--- tw2/protovis/core/widgets.py
+++ tw2/protovis/core/widgets.py
@@ -3,6 +3,7 @@ TODO
 """
 
 import tw2.core as twc
+from tw2.core.resources import encoder
 from tw2.protovis.core import resources as res
 
 class PVPanel(twc.Widget):
@@ -11,4 +12,24 @@ class PVPanel(twc.Widget):
    
     width = twc.Param('(string) widget width', default='150', attribute=True)
     height = twc.Param('(string) widget height', default='150', attribute=True)
+    _pv_prop_funcs = twc.Variable('(list) of JSSymbols', default=[])
+
+    def handlerFunctionClosure(self, name):
+        def handlerFunction(*args, **kwargs):
+            if kwargs:
+                raise ValueError, "keyword arguments are disallowed"
+            f = twc.JSSymbol(src=".%s(%s)" % (name, encoder.encode(args)[1:-1]))
+            # TODO -- need more robust check here.
+            # BUG on multiple page reloads, this still gets duplicates
+            if f not in self._pv_prop_funcs:
+                self._pv_prop_funcs.append(f)
+        return handlerFunction
+
+    def __getattr__(self, name):
+        return self.handlerFunctionClosure(name)
+
+    def prepare(self):
+        super(PVPanel, self).prepare()
+        # -- this is just a test
+        self.widthx(50, 'foo', twc.JSSymbol(src='function(){return "f"}'))
 
